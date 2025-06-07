e51d5660a8208dfe6fa00baa35368cbf873dd579
Mamta-Gaddam <mamtagaddam2803@gmail.com>
2025-06-07 01:22:49 -0700

    stringconcat

---
+++ stringconcat.py
@@ -0,0 +1,10 @@
+#Concatenate the string 'Thirty', 'Days', 'Of', 'Python' to a single string, 'Thirty Days Of Python'.
+
+name1="Thirty"
+space1=" "
+name2="Days"
+space2=" "
+name3="of"
+space3=" "
+name4="Python"
+print(name1 + space1 + name2 + space2 + name3 + space3 + name4)
